683ffafc8a8324ae99ec97a9e2b20e83cf6c72d1
Bhushan <pbhushan246@gmail.com>
2019-05-09 10:34:37 +0530

    Initial commit

---
+++ app/src/main/java/com/eijs/creditscore/MainActivity.java
@@ -0,0 +1,47 @@
+package com.eijs.creditscore;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_account:
+                    mTextMessage.setText(R.string.account);
+                    return true;
+                case R.id.navigation_score:
+                    mTextMessage.setText(R.string.score);
+                    return true;
+                case R.id.navigation_history:
+                    mTextMessage.setText(R.string.history);
+                    return true;
+                case R.id.navigation_logout:
+                    mTextMessage.setText(R.string.logout);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
