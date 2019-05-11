03c0ae01ed1efdc95a98f49022abf2f79373c9ee
Bhushan <pbhushan246@gmail.com>
2019-05-11 11:43:06 +0530

    commit on 11-05-2019

--- app/src/main/java/com/eijs/creditscore/java/Frag_history.java
+++ app/src/main/java/com/eijs/creditscore/java/Frag_history.java
@@ -1,22 +1,73 @@
 package com.eijs.creditscore.java;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.eijs.creditscore.R;
+import com.eijs.creditscore.others.ViewDialog;
 
 
 public class Frag_history extends Fragment {
 
-
+    View view;
+    RecyclerView rv_hist;
+    ViewDialog progressDialog;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_frag_history, container, false);
+        view = inflater.inflate(R.layout.fragment_frag_history, container, false);
+        progressDialog=new ViewDialog(getActivity());
+        rv_hist = view.findViewById(R.id.rv_history);
+        rv_hist.setNestedScrollingEnabled(false);
+        rv_hist.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        rv_hist.setAdapter(new RecyclerView.Adapter() {
+            @NonNull
+            @Override
+            public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+                View view=LayoutInflater.from(getActivity()).inflate(R.layout.history_adapter, viewGroup,false);
+                Holder holder=new Holder(view);
+                return holder;
+            }
+
+            @Override
+            public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+                final Holder myHolder= (Holder) viewHolder;
+
+                myHolder.itemView.setTag(i);
+                myHolder.itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                    }
+                });
+
+            }
+
+            @Override
+            public int getItemCount() {
+                return 20;
+            }
+            class Holder extends RecyclerView.ViewHolder {
+                TextView name,hqname;
+                public Holder(@NonNull View itemView) {
+                    super(itemView);
+                    name = itemView.findViewById(R.id.name);
+                    hqname = itemView.findViewById(R.id.hqname);
+                }
+            } }
+        );
+
+        //getEmplist();
+        return view;
     }
 
 }
--- app/src/main/java/com/eijs/creditscore/java/Frag_scentry.java
+++ app/src/main/java/com/eijs/creditscore/java/Frag_scentry.java
@@ -1,21 +1,76 @@
 package com.eijs.creditscore.java;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.eijs.creditscore.R;
+import com.eijs.creditscore.others.ViewDialog;
 
 
 public class Frag_scentry extends Fragment {
+
+    View view;
+    RecyclerView rv_entry;
+    ViewDialog progressDialog;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_frag_scentry, container, false);
+        view = inflater.inflate(R.layout.fragment_frag_scentry, container, false);
+        progressDialog=new ViewDialog(getActivity());
+        rv_entry = view.findViewById(R.id.rv_entry);
+        rv_entry.setNestedScrollingEnabled(false);
+        rv_entry.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        rv_entry.setAdapter(new RecyclerView.Adapter() {
+            @NonNull
+            @Override
+            public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+                View view=LayoutInflater.from(getActivity()).inflate(R.layout.entry_adapter, viewGroup,false);
+                Holder holder=new Holder(view);
+                return holder;
+            }
+
+            @Override
+            public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+                final Holder myHolder= (Holder) viewHolder;
+
+                myHolder.itemView.setTag(i);
+                myHolder.itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                    }
+                });
+
+            }
+
+            @Override
+            public int getItemCount() {
+                return 20;
+            }
+            class Holder extends RecyclerView.ViewHolder {
+                TextView name,hqname;
+                public Holder(@NonNull View itemView) {
+                    super(itemView);
+                    name = itemView.findViewById(R.id.name);
+                    hqname = itemView.findViewById(R.id.hqname);
+                }
+            } }
+        );
+
+        //getEmplist();
+        return view;
     }
 
 }
